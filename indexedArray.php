fe294ae8365442e28e4d5217c6c40431e7c26957
syhirrh <syasyahirah31@gmail.com>
2022-05-12 13:36:33 +0800

    foreach commit

--- indexedArray.php
+++ indexedArray.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <?php
-    $month = array ('January', 'February', 'March', 'April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');     
+    $month = array ("January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December");     
     sort($month);
     foreach($month as $key => $val)
     {
