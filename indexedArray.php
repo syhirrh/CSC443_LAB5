83856b41a2304e9403aca5cb613ca437a2608bab
syhirrh <syasyahirah31@gmail.com>
2022-05-12 13:25:08 +0800

    sort commit

--- indexedArray.php
+++ indexedArray.php
@@ -8,9 +8,7 @@
 </head>
 <body>
 <?php
-    $month = array ('January', 'February', 'March', 'April',
-        'May', 'June', 'July', 'August', 'September', 
-        'October', 'November', 'December');     
+    $month = array ('January', 'February', 'March', 'April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');     
     sort($month);
     for($i=0; $i<=11; $i++)
     {
